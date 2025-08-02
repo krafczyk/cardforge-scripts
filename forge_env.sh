898d802fa317bf0c756ce3381a3618858f051b21
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2025-08-01 23:13:15 -0500

    Update forge script so it works with zsh too

--- forge_env.sh
+++ forge_env.sh
@@ -1,20 +1,21 @@
 export FORGE_PROJECT_ROOT="$(realpath $(dirname $(realpath $0)))/.."
 
 validate_variable() {
-  local var_name="$1"
-  local default_value="$2"
-  # indirect expansion to get the current value of $var_name
-  local value="${!var_name:-}"
+  local var_name="$1" default_value="$2" value
 
-  if [[ -z "$value" ]]; then
+  # indirect expansion to get the current value of $var_name
+  eval "value=\${$var_name:-}"
+
+  if [[ -z $value ]]; then
     # not set or empty → assign default
-    export "$var_name"="$default_value"
+    eval "export $var_name=\"\$default_value\""
   elif [[ ! -d "$value" ]]; then
     # set but not a valid dir → error out
     echo "Error: $var_name is set to an invalid path: $value" >&2
-    exit 1
   fi
 }
 
 validate_variable "JAVA_HOME" "/usr/lib/jvm/java-17-openjdk/"
 validate_variable "ANDROID_SDK_ROOT" "$FORGE_PROJECT_ROOT/Android/Sdk"
+
+export PATH=${FORGE_PROJECT_ROOT}/apache-maven-3.8.1/bin:${PATH}
